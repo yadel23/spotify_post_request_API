0f81f9ee7688a2f0ceeb73e76de9e2c4d2de6d8b
Yadel Negash <yadelnegash10@gmail.com>
2021-06-29 20:45:17 +0000

    parsed JSON file to useful artist info

--- apiProject.py
+++ apiProject.py
@@ -1,8 +1,9 @@
 import requests
 import spotipy
+import json
 
-url = 'https://accounts.spotify.com/api/token'
-client_id = 'cc9846731cd64c33befaa1beb31f4886'
+url = 'https://accounts.spotify.com/api/token' 
+client_id = 'cc9846731cd64c33befaa1beb31f4886' 
 client_secret = 'a0c7aaad594a4406b6563b9b2a2e28b7'
 response = requests.post(url, {
   'grant_type': 'client_credentials',
@@ -10,6 +11,21 @@ response = requests.post(url, {
   'client_secret': client_secret,
 })
 
+auth_response_data = response.json()
 
-print(response.status_code)
-print(response.json())
+access_token = auth_response_data['access_token']
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+base_url = 'https://api.spotify.com/v1/'
+track_id = '4vDBJeeQCbhP9FaPPMsYkY?si=992cc74ea4564ff6'
+artist_id = '3TVXtAsR1Inumwj472S9r4?si=z1z0cXVQTtefzolOGqO6Zw&dl_branch=1'
+
+r = requests.get(base_url + 'artists/' + artist_id, headers=headers)
+request_jsonformat = r.json()
+
+for key, value in vs.items():
+  if key == 'name' or key == 'popularity' or key == 'genres':
+   print(key, ":", value)   
+  
