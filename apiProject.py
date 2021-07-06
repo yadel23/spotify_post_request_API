8ad7461f3221295ff5c259c28679522170444114
Yadel Negash <yadelnegash10@gmail.com>
2021-07-06 02:48:36 +0000

    added more unit tests

--- apiProject.py
+++ apiProject.py
@@ -2,6 +2,17 @@ import requests, spotipy, json, sqlalchemy
 import pandas as pd
 from sqlalchemy import create_engine
 
+
+def user_inputLink():
+    try:
+#       user_artistLink = input('Please enter link here \n')
+      user_artistLink = 'https://open.spotify.com/artist/1ThoqLcyIYvZn7iWbj8fsj?si=babe35098fe141a2'
+      artist_id = user_artistLink.split('artist/')
+      return artist_id[-1]
+    except (RuntimeError, TypeError, NameError):
+      return null
+  
+# hard coded does not need tests
 def instructions():
     print(''' 
       1. Go to spotify and go to the artist of your choosing
@@ -12,7 +23,7 @@ def instructions():
       ''')
 
 
-#hard coded does not need tests
+# hard coded does not need tests
 def heardcoded_apiInfo():
     url = 'https://accounts.spotify.com/api/token' 
     client_id = 'cc9846731cd64c33befaa1beb31f4886' 
@@ -25,63 +36,38 @@ def heardcoded_apiInfo():
     return response
 
 
-# auth_response_data = response.json()
-# access_token = auth_response_data['access_token']
-# headers = {
-#     'Authorization': 'Bearer {token}'.format(token=access_token)
-# }
-
-#base_url = 'https://api.spotify.com/v1/'
-
-#Test 3. test to handle if the input is correct
-def user_inputLink():
-    try:
-#       user_artistLink = input('Please enter link here \n')
-      user_artistLink = 'https://open.spotify.com/artist/1ThoqLcyIYvZn7iWbj8fsj?si=babe35098fe141a2'
-      artist_id = user_artistLink.split('artist/')
-      return artist_id[-1]
-    except (RuntimeError, TypeError, NameError):
-      return null
-      
-
-
-#Test 1. make test to see vaild connection to api
-#Test 2. make test to see if json output is expected
-
 def get_json(special_link, response):
     auth_response_data = response.json()
     access_token = auth_response_data['access_token']
     headers = {
         'Authorization': 'Bearer {token}'.format(token=access_token)
-    }
-    
+    }   
     r = requests.get('https://api.spotify.com/v1/' + 'artists/' + special_link, headers=headers)
     request_jsonformat = r.json()
-#     print(request_jsonformat)
-#     print('\n\n\n\n\n\n\n')
     return request_jsonformat
 
+  
 def create_dic(request_jsonformat):
     artist_dic = {}
     for key, value in request_jsonformat.items():
       if key == 'name' or key == 'popularity' or key == 'genres':
          artist_dic[key] = value
-#     print(artist_dic)
-#     print('\n\n\n\n\n')
+    #print(artist_dic)
     return artist_dic
 
+  
 def build_dataframe(artist_dic):
     col_names = ['name', 'popularity', 'genres']
     df = pd.DataFrame(columns = col_names)
     df.loc[len(df.index)] = [artist_dic['name'], artist_dic['popularity'], str(artist_dic['genres'])]
-#     print(df)
-#     print('\n\n build_dataframe working \n\n\n\n')
     return df
-  
+
+
 def set_dataframe(df):
     engine = create_engine('mysql://root:codio@localhost/artists')
     df.to_sql('artists_data', con=engine, if_exists = 'append', index=True)
 
+    
 # os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '+database_name+';"')
 # #create database if it does not exist
 # os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '+database_name+';"')
@@ -96,5 +82,5 @@ def main():
     data_frame = build_dataframe(dic)
     set_dataframe(data_frame)    
     
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
--- apiProjectTest.py
+++ apiProjectTest.py
@@ -1,9 +1,59 @@
-import unittest
-from apiProject import user_inputLink
+import unittest, re
+from apiProject import user_inputLink, get_json, heardcoded_apiInfo, create_dic
 
 class TestFileName(unittest.TestCase):
     def test_function1(self):
-        self.assertTrue(len(user_inputLink()) > 5)
+        string_val = user_inputLink()
+        
+        self.assertTrue(len(string_val) > 20)
+        
+        special_char_isTrue = False
+        special_char = re.compile('[@_!#$%^&*()<>?/\|}{~:]')       
+        if(special_char.search(string_val) != None):
+          special_char_isTrue = True
+        self.assertTrue(special_char_isTrue)
+        
+        nums_res = any(chr.isdigit() for chr in string_val)
+        self.assertTrue(nums_res)
+        
+        has_upper_case = any(chr.upper() for chr in string_val)
+        self.assertTrue(has_upper_case)
+        
+        has_lower_case = any(chr.lower() for chr in string_val)
+        self.assertTrue(has_lower_case)
+
+    def test_function2(self):
+        string_val = user_inputLink()
+        is_json = get_json(string_val, heardcoded_apiInfo())
+        self.assertTrue(type(is_json), dict)
+        
+    def test_function3(self):
+        string_val = user_inputLink()
+        jsonformat = get_json(string_val, heardcoded_apiInfo())
+        dic_list = create_dic(jsonformat)
+        key_list = dic_list.keys()
+
+
+        contains_names = False 
+        contains_geners = False
+        contains_pop_score = False
+
+        if 'name' in key_list:
+          contains_names = True
+          
+        if 'popularity' in key_list:
+          contains_pop_score = True
+          
+        if 'genres' in key_list:
+          contains_geners = True
+          
+        self.assertTrue(type(dic_list), dict)
+        self.assertTrue(contains_names)
+        self.assertTrue(contains_geners)
+        self.assertTrue(contains_pop_score)
+          
+        
+
       
 
 if __name__ == '__main__':
